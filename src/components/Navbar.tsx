f2f75309526fc369d68ceef6d66d61c686e84b46
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2025-09-22 21:35:33 +0500

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navbar-container'>
+            <div className='iner-content'>
+                <div className="logo-wrapper">
+
+                </div>
+                <div className="nav-links-wrapper">
+                    
+                </div>
+                <div className="nav-btns">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
