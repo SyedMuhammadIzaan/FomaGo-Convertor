b26518fe27aee7250dd0f24e9acc078cc2bcfe78
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2025-10-22 22:54:09 +0500

    set the ard styling in keyFetaure component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,18 +11,17 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ key, data }) => {
     console.log("Key and feature:", key, data)
     return (
-        <div className='cardContainer border-2 border-purple-400 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300'>
-            <div className="cardContent border-2">
-                <div className="iconWrapper border-2 mb-2">
+        <div className='cardContainer border-2 border-grey-400 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300'>
+            <div className="cardContent">
+                <div className="iconWrapper mb-2 flex flex-col items-center">
                     {/* Placeholder for icon */}
-                    <div className={`icon-placeholder ${data.icon} w-12 h-12 bg-gray-200 flex items-center justify-center rounded-full`}>
-                        <div className="titleWrapper"><span>{data.title}</span></div>
-                        <div className="descriptionWrapper">
-                            <span>{data.desc}</span>
-                        </div>
+                    <div className={`icon-placeholder ${data.icon} w-12 h-12 rounded-full`}>
+                    </div>
+                    <div className="titleWrapper"><span className='text-lg'>{data.title}</span></div>
+                    <div className="descriptionWrapper">
+                        <span className='text-base'>{data.desc}</span>
                     </div>
                 </div>
-                )
             </div>
         </div>
     )
