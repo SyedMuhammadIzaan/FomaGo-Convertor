6e83d39e07c2ea1007a71fbb386d991a9a49df86
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2025-10-23 23:34:31 +0500

    add text in the component

--- src/components/layout/useOfbackgroundRemover.tsx
+++ src/components/layout/useOfbackgroundRemover.tsx
@@ -8,11 +8,26 @@ const UseOfBackgroundRemover = () => {
                 <p className="text-md text-center">
                     The Background Remover tool is designed to help users easily remove backgrounds from images, making them more versatile for various applications. Here are some common uses of the Background Remover:
                 </p>
-                <div className="backgroundremoverGridContainer w-full flex flex-row border-2 p-4">
-                    <div className="colContainer border-2 w-1/2 border-grey-300">
+                <div className="backgroundremoverGridContainer w-full flex flex-col border-2 p-4 md:flex-row lg:flex-row xl:flex-row">
+                    <div className="colContainer border-2 w-full md:w-1/2 lg:w-1/2 border-grey-300">
                         <img className="w-full" src={BackgroundRemoverImage} alt="" />
                     </div>
-                    <div className="colContainer border-2 w-1/2 border-grey-300"></div>
+                    <div className="colContainer border-2 w-full md:w-1/2 lg:w-1/2 border-grey-300 p-1">
+                        <div className="useOfBackgroundRemoverTitle mb-4">
+                            <h3 className="text-center font-medium">How to Use the Background Remover</h3>
+                        </div>
+                        <div className="stepsToRemoveBackground">
+                            <ul>
+                                <li>Click on the “Upload” button or drag & drop your image into the upload box.</li>
+                                <li>The system will automatically detect and
+                                    remove the background.</li>
+                                <li>A preview of your image with the background removed will be shown.</li>
+                                <li>Click on the Download button to save
+                                    your image (PNG/JPG with transparent or
+                                    white background).</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
